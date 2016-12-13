660382b9e7f9e47e5ede07ac9e819a84240431c2
zanellaigor <zanellaigor@Lab06_15.itseinaudi.local>
2016-12-13 09:59:29 +0100

    as

--- SocketGUI/src/BotIgoraz/Bot.java
+++ SocketGUI/src/BotIgoraz/Bot.java
@@ -6,36 +6,42 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Bot extends Thread{
+import Zemian.IMessageReceiver;
+import Zemian.Server;
+
+public class Bot extends Thread implements IMessageReceiver {
 	ServerSocket ss;
 	Socket socket;
+	Server server;
 	Main main;
 	
 	public Bot(Main main){
 		this.main = main;
+		server = new Server(this);
 	}
-	
-	public void run(){
-		try {
-			ss = new ServerSocket(9080);
-			socket = ss.accept();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+	@Override
+	public void setHost(String host) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receive(String from, String message) {
+		// TODO Auto-generated method stub
+		main.putList(from+": "+message);
+		message.toLowerCase();
+		switch(message){
+		case "come ti chiami?":
+			break;
+		case "":
+			break;
 		}
-		while(true){
-			InputStreamReader iSr;
-			try {
-				iSr = new InputStreamReader(socket.getInputStream());
-				BufferedReader in = new BufferedReader(iSr);
-				String message = in.readLine();
-				System.out.println("Il server riceve:" + message);
-				String s[] = message.split(": ");
-				main.putList(s[1]);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	}
+
+	@Override
+	public String getHost() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
